622d4840659a899733c0fb75621e84df8f83c3b5
VictorSuarezVaraDoWhile <55837304+VictorSuarezVara@users.noreply.github.com>
2020-11-09 15:34:16 +0100

    Test de partida anomenat testInputToCoords desenvolupat

--- src/test/java/BuscaminasProject/PartidaTest.java
+++ src/test/java/BuscaminasProject/PartidaTest.java
@@ -29,8 +29,64 @@ public class PartidaTest {
 
 	
 	@Test
-	public void testInputToCoords() { 
-		fail("Not yet implemented");
+	public void testInputToCoords() 
+	{ 
+		Partida p = new Partida();
+		
+		
+		String listAuxInput[]= {"1A","3D","4B","6F"};
+		int listAuxCoorectResult[][]= {{0,0},{2,3},{3,2},{5,5}};
+		
+		for (int i = 0; i < listAuxInput.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput(i), listAuxCoorectResult[i]));
+		}
+		
+		
+		int errorReturn[]= {-1,-1};
+		
+		//ERRORS PER COORD ALFABET
+		String listAuxInput2[]= {"1J","1Z","1$","1€","0A","-4A","1a","3d","4b","6f"};
+		
+		for (int i = 0; i < listAuxInput2.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput2(i), errorReturn));
+		}
+		
+		//ERRORS PER COORD NUMERICA	
+		String listAuxInput2[]= {"0A","-4A","10A","100A"};
+		
+		for (int i = 0; i < listAuxInput2.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput2(i), errorReturn));
+		}
+		
+		//ERRORS PER VALORS MOLT EXTRANYS
+		String listAuxInput2[]= {"11111111111111111111111","AAAAAAAAAAAAAAAAAAAAAAAA"};
+		
+		for (int i = 0; i < listAuxInput2.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput2(i), errorReturn));
+		}
+		
+		//ERRORS PER INERCANVI DE COORDS 	
+		String listAuxInput2[]= {"1A","DR","B4","6F"};
+		
+		for (int i = 0; i < listAuxInput2.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput2(i), errorReturn));
+		}
+				
+		//ERRORS PER CASSOS EXTRANGS
+		String listAuxInput2[]= {"1A1","A11","11A","AA1","A1A","1AA"};
+		
+		for (int i = 0; i < listAuxInput2.length; i++) 
+		{
+			assertEquals(p.inputToCoords(listAuxInput2(i), errorReturn));
+		}
+		
+		
+		
 	}
 	
 	
