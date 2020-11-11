9138e376fe9a3b66db21cd7d592f9f1489082726
VictorSuarezVaraDoWhile <55837304+VictorSuarezVara@users.noreply.github.com>
2020-11-11 11:31:24 +0100

    Metode Input to cords finalitzat

--- src/main/java/BuscaminasProject/Partida.java
+++ src/main/java/BuscaminasProject/Partida.java
@@ -1,6 +1,8 @@
 package BuscaminasProject;
 
-java.lang.String
+import java.lang.String;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Partida {//MODEL
 
@@ -10,13 +12,11 @@ public class Partida {//MODEL
 	 * i algun simbol per marcar si hi ha posat una 'bandera' marcant que hi ha una bomba
 	 */
 	private int[][] taulerVista;
-	private int puntuacio;
 	private int bombesTotals;
 	
 	public Partida(){
 		taulerReal = new Tauler();
 		bombesTotals = taulerReal.countBombes();
-		puntuacio = 0;
 	}
 	
 	public int processaMoviment(String input) { //return false if bomb is hit
@@ -33,25 +33,64 @@ public class Partida {//MODEL
 
 		return 0;
 	}
-	
-	private int[] inputToCoords(String input) 
+	/**
+	 * 
+	 * @param input
+	 * @return an int array of length 3, first element is x coordinate,
+	 * 		   second element is y coordinate and third is input is to 
+	 * 		   flag(1) or not(0)
+	 */
+	public int[] inputToCoords(String input) 
 	{
 		
 		//Method that change the input recibed to coords estandarized with the tauler 
+		int coords[] = { -1, -1, -1};
+		Pattern patro = Pattern.compile("^([-]?[A-Za-z]{1}[1-9]{1}[/]{0,1})$"); //regex to accept only valid input
+		Matcher coincideix = patro.matcher(input);
+		if(coincideix.matches()) { //input en el format correcte
 		
-		int coords[] = {0,0};
+			boolean correctValue=true;
+			
+			int x = (int) input.toUpperCase().charAt(0) - 'A';
+			
+			
+			
+			int y = (int) input.toUpperCase().charAt(1) - '0' -1;
+			if (x<0 || x>= taulerReal.getHeight()||y<0 || y>= taulerReal.getWidth()) {
+				correctValue=false;
+			}
+			
+			if(correctValue) 
+			{
+			
+				coords[0] = x;
+				coords[1] = y;
+			
+			
+				if(input.length() == 3) 
+				{ 
+					// with flag
+					coords[2] = 1;
+				}else 
+				{
+					//not a flag
+					coords[2] = 0;
+				}
+			
+			}
+		
+		}
 		return coords;
-		
 	}
 	
-	private int getValueOfTauler(int coordx,int coordy)
+	public int getValueOfTauler(int coordx,int coordy)
 	{
 		//Method that gets the value of tauler that the user has input recibed in coordx and coordy
 
 		return 1;
 	}
 	
-	private boolean checkGameIsWin()
+	public boolean checkGameIsWin()
 	{
 		//Method that analyzes if the game is win
 		//if it's win returns true
@@ -60,7 +99,7 @@ public class Partida {//MODEL
 		return true;
 	}
 	
-	private void updateVistaTauler(int coordx,int coordy)
+	public void updateVistaTauler(int coordx,int coordy)
 	{
 		//Method that updates the taulr that sees the user
 		
@@ -79,9 +118,6 @@ public class Partida {//MODEL
 		return taulerVista;
 	}
 	
-	//public int getPuntuacio() {
-	//	return -1;
-	//}
 	
 	public int getBombesTotals() {
 		return -1;
