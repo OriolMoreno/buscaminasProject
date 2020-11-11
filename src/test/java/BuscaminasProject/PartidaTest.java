67165f6be8dc1de15dbb1ba6b1f0f660d374db28
VictorSuarezVaraDoWhile <55837304+VictorSuarezVara@users.noreply.github.com>
2020-11-11 11:43:10 +0100

    Test testGetValueOfTauler done before doing the implementation of the method

--- src/test/java/BuscaminasProject/PartidaTest.java
+++ src/test/java/BuscaminasProject/PartidaTest.java
@@ -186,7 +186,7 @@ public class PartidaTest {
 		}
 
 		//PARTICIÓ EQUIVALENT ERROR EN COORD NUMERICA (amb valors límits  i frontera)
-		String listAuxInput3[]= {"A0/","-A4/","A10/","A100/"};
+		String listAuxInput3[]= {"A0/","-A4/","A9/","A10/","A100/"};
 		
 		for (int i = 0; i < listAuxInput3.length; i++) 
 		{
@@ -232,7 +232,30 @@ public class PartidaTest {
 	}
 	//@Test
 	public void testGetValueOfTauler() { 
-		fail("Not yet implemented");
+		Partida p = new Partida();
+		
+		
+		//La única partició son els cassos de proba amb valors coorectes ja que en el metode inputToCords ja 
+		// s'ha validat que les coordenades siguin pertanyents al tauler de la partida
+		
+		//Values frontera i possibles values problemàtics del mig
+		assertEquals(p.getValueOfTauler(0, 0), 1);
+		assertEquals(p.getValueOfTauler(8, 7), 0);
+		assertEquals(p.getValueOfTauler(0, 4), 1);
+		assertEquals(p.getValueOfTauler(3, 7), -1);
+		assertEquals(p.getValueOfTauler(5, 6), -1);
+		
+		//THE MOCK TAULER
+		//		{1, -1,  2,  2,  1,  1,  0,  0},
+		//		{1,  2, -1,  3, -1,  2,  1,  1},
+		//		{1,  2,  4, -1,  3,  3, -1,  3},
+		//		{1, -1,  3, -1,  3,  3, -1, -1},
+		//		{1,  2,  3,  3,  3, -1,  5, -1},
+		//		{1,  2, -1,  2, -1,  3, -1,  2},
+		//		{1, -1,  2,  2,  2,  3,  2,  1},
+		//		{2,  2,  3,  2,  4, -1,  2,  0},
+		//		{1, -1,  2, -1, -1, -1,  2,  0}
+				
 	}
 	
 	
